22292f8ef6d6d194d5422994d619f8031604c976
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-21 17:03:07 +0200

    Add edit functionality styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,22 +2,53 @@ import React from 'react';
 import styles from './TodoItem.module.css';
 
 class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  };
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  };
+
+  handleUpdatedDone = (e) => {
+    e.preventDefault()
+    this.setState({ editing: false });
+  };
+
   render() {
     const { completed, id, title } = this.props.todo;
+    let viewMode = {};
+    let editMode = {};
+
+    this.state.editing
+      ? (viewMode.display = 'none')
+      : (editMode.display = 'none');
+
     return (
       <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type='checkbox'
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)} className={styles.delete}>Delete</button>
+          <span className={completed ? styles.completed : null}>{title}</span>
+        </div>
+        <form onSubmit={this.handleUpdatedDone} className={styles['textInput', 'formContainer']}  style={editMode}>
         <input
-          type='checkbox'
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
+          type='text'
+          className='input-text'
+          value={title}
+          onChange={(e) => {
+            this.props.setUpdate(e.target.value, id);
+          }}
         />
-        <button onClick={() => this.props.deleteTodoProps(id)}>
-          Delete
-        </button>
-        <span className={completed ? styles.completed : null}>
-        {title}
-        </span>
+        <button className='input-submit'>Save</button>
+        </form>
       </li>
     );
   }

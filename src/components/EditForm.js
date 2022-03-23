686e91c8ecaeb0b6ab0dccca53ee1b405788eb39
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-23 07:41:09 +0200

    Refactor all components using hooks && use useEffect for cleanup when components get unmounted

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './TodoItem.module.css';
 
-class EditForm extends Component {
-  constructor(props) {
-    super(props);
-    this.textInput = React.createRef();  }
+const EditForm = (props) => {
+  const textInput = useRef(null);
 
-    componentDidMount() {
-      this.textInput.current.focus();
-    }
+  useEffect(() => {
+    return () => {};
+  }, []);
 
-  render() {
-    return (
-      <form
-        onSubmit={this.props.handleUpdatedDone}
-        onBlur={this.props.handleUpdatedDone}
-        className={styles[('textInput', 'formContainer')]}
-      >
-        <input 
-        ref={this.textInput }
-          type='text'
-          className='input-text'
-          value={this.props.title}
-          onChange={(e) => {
-            this.props.setUpdate(e.target.value, this.props.identifier);
-          }}
-        />
-        <button className='input-submit'>Save</button>
-      </form>
-    );
-  }
-}
+  useEffect(() => {
+    textInput.current.focus();
+  }, []);
+
+  return (
+    <form
+      onSubmit={props.handleUpdatedDone}
+      onBlur={props.handleUpdatedDone}
+      className={styles[('textInput', 'formContainer')]}
+    >
+      <input
+        ref={textInput}
+        type='text'
+        className='input-text'
+        value={props.title}
+        onChange={(e) => {
+          props.setUpdate(e.target.value, props.identifier);
+        }}
+      />
+      <button className='input-submit'>Save</button>
+    </form>
+  );
+};
 
 export default EditForm;
